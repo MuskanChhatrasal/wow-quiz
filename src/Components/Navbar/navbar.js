b1d9c94cf57f1655598f46c060235aeb7f8bfcf3
Muskan <muskanchhatrasal@gmail.com>
2022-05-18 20:15:01 +0530

    Done - Navbar

---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header
+      className="header cont-shadow"
+      style={{ width: "96rem", backgroundColor: "white" }}
+    >
+      <div className="logos">
+        <Link to="/">
+          <a>
+            W <span className="logos-o">O</span>W Quiz
+            <span className="logos-excla">!</span>
+          </a>
+        </Link>
+      </div>
+      <ul className="header-nav">
+        <li style={{ marginTop: "0.25rem", cursor: "pointer" }}>
+          <li style={{ cursor: "pointer" }}>
+            <a>
+              <i
+                class="fas fa-sign-out-alt"
+                style={{ marginRight: "0.5rem" }}
+              ></i>
+              Github
+            </a>
+          </li>
+        </li>
+
+        <li>
+          <button className="menu">
+            <i className="bi bi-list"></i>
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
