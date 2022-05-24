4163debe5d01ca609829d424c3f8b949fc71a8ea
Muskan <muskanchhatrasal@gmail.com>
2022-05-24 08:13:15 +0530

    Bug fixed

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const [user, setUser] = useState({});
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -20,7 +20,7 @@ const ProtectedRoute = ({ children }) => {
     return <Navigate to="/" />;
   }
 
-  return children;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
